e4727997e298b0b664182ad0a33cc77c1ecdcc0b
Freda Hon <freda.hon@gmail.com>
2020-09-09 20:24:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,11 +4,7 @@
 def join_nested_strings(src)
   row_index = 0
   while row_index < src.length do
-    element_index = 0
-    while element_index < src[row_index].length do
-     src[row_index][element_index].grep(String)
-      element_index += 1
-    end
-  row_index += 1  
+    src[row_index].grep(String)
+    row_index += 1  
   end
 end
